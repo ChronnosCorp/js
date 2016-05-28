3479fe43a3d91e3d161e2797d35fcaa49cd6d789
Dennyfentow <megabradock@gmail.com>
2016-05-28 01:19:46 -0300

    Add files via upload

---
+++ verificacaoEstoque.js
@@ -0,0 +1,47 @@
+/**
+ * Radios
+ */
+var radios = document.getElementsByName("MEstoque");
+function MudarEstadoTodos() {
+    MudarEstadoIncluir('DivInserir');
+    MudarEstadoAlterar('DivAlterar');
+    MudarEstadoExluir('DivExcluir');
+    MudarEstadoConsultar('DivConsultar');
+}
+function MudarEstadoIncluir(el) {
+    if (radios[0].checked) {
+        document.getElementById(el).style.display = 'block';
+    } else {
+        document.getElementById(el).style.display = 'none';
+    }
+}
+function MudarEstadoAlterar(el) {
+    
+    if (radios[1].checked) {
+        document.getElementById(el).style.display = 'block';
+    } else {
+        document.getElementById(el).style.display = 'none';
+    }
+}
+function MudarEstadoExluir(el) {
+    
+    if (radios[2].checked) {
+        document.getElementById(el).style.display = 'block';
+    } else {
+        document.getElementById(el).style.display = 'none';
+    }
+}
+function MudarEstadoConsultar(el) {
+    
+    if (radios[3].checked) {
+        document.getElementById(el).style.display = 'block';
+    } else {
+        document.getElementById(el).style.display = 'none';
+    }
+}
+function VoltarPrincipal(){
+    window.open("../index.html", '_parent'); 
+}
+function VoltarEstoque(){
+    window.open("../ManutencaoEstoque.html", '_parent'); 
+}
